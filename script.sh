3ef5d5b79e4e22de559c390d6a05415b4df40e77
Gregory Dillon <gregorydillon@gmail.com>
2016-03-28 16:13:49 -0700

    updating readme  describe issues

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
-curl "https://data.sfgov.org/resource/p8zf-gnjt.csv?\$LIMIT=10000000&\$ORDER=created_date%20DESC" | head -n 60000 > raw.csv"
+curl 'https://data.sfgov.org/resource/p8zf-gnjt.csv?\$LIMIT=10000000&\$ORDER=created_date%20DESC' | head -n 60000 > raw.csv
 
 node transform.js raw.csv cleaned.csv
