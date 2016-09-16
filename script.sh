4af518e3e74df40671edee62d9d8b07b2068d468
Gregory Dillon <gregorydillon@gmail.com>
2016-09-16 23:53:03 +0000

    modified script.sh to also create a csv version

--- script.sh
+++ script.sh
@@ -1,2 +1,3 @@
 
- wget -O reports.json "http://data.sfgov.org/resource/qer8-n8u9.json"                     
+wget -O reports.json "http://data.sfgov.org/resource/qer8-n8u9.json"                     
+curl "http://data/sfgov.org/resource/qer8-n8u9" | head -n 1001 > reports.csv
