307b2872df1a542c8fb0de1dd2734a7693dc3a62
Gregory Dillon <gregorydillon@gmail.com>
2016-09-17 00:57:11 +0000

    added shell script for running ipython. jupyter notebooks

---
+++ install_jupyter-notebook.sh
@@ -0,0 +1,4 @@
+ jupyter notebook --ip=0.0.0.0 --port=8080 --no-browser
+ # open browser at the port shown
+ # usually  http://0.0.0.0:8080/
+ # becomes  http://getter311-gregorydillon-1.c9users.io:8080/tree
