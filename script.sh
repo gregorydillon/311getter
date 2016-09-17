9db589bc829d480f588715d076d20005205a21e0
Gregory Dillon <gregorydillon@gmail.com>
2016-09-17 00:03:02 +0000

    corrected script.sh for report.csv:

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 
 wget -O reports.json "http://data.sfgov.org/resource/qer8-n8u9.json"                     
-curl "http://data/sfgov.org/resource/qer8-n8u9" | head -n 1001 > reports.csv
+curl "http://data.sfgov.org/resource/qer8-n8u9.csv" | head -n 1000 > reports.csv
