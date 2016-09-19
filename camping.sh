2dc59781f984908d7a1e9e0c65152c1fbc4f1415
Gregory Dillon <gregorydillon@gmail.com>
2016-09-19 18:47:46 +0000

    added camping as a data pull

---
+++ camping.sh
@@ -0,0 +1,3 @@
+
+wget -O reports.json "http://data.sfgov.org/resource/gbya-e2d3.json"                     
+curl "http://data.sfgov.org/resource/qer8-n8u9.csv" | head -n 1000 > reports.csv
--- movie.sh
+++ movie.sh
@@ -1,4 +1,5 @@
 wget -ximediaURLs.txt
 find -name "*.jpg" | gawk 'BEGIN{ a=1 }{ printf "mv %s %04d.jpg\n", $0, a++ }' | bash
-avconv -framerate 2 -f image2 -i %04d.jpg  coffee.mp4
+avconv -framerate 1/2  -i %04d.jpg video.webm
+# avconv -framerate 2 -f image2 -i %04d.jpg  coffee.mp4
 # avconv -framerate 1 -i %04d.jpg -c:v libx264 -r 30 out.mp4
