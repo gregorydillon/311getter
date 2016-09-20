9e82c25634fd106be931c2d79a22ab9009d82193
Gregory Dillon <gregorydillon@gmail.com>
2016-09-20 07:11:52 +0000

    start of scene of location

---
+++ grabMapnView.sh
@@ -0,0 +1,28 @@
+wget "https://maps.googleapis.com/maps/api/staticmap?markers=37.7728249509386,-122.406590353307&zoom=12&size=800x600" -O myMap.jpg
+wget "https://maps.googleapis.com/maps/api/staticmap?markers=37.7728249509386,-122.406590353307&zoom=12&size=800x600" -O myMap.jpg
+wget "https://maps.googleapis.com/maps/api/streetview?size=800x600&location=37.7728249509386,-122.406590353307
+&fov=40&heading=0&pitch=4" -O myStreetView01.jpg
+
+wget "https://maps.googleapis.com/maps/api/streetview?size=800x600&location=37.7728249509386,-122.406590353307
+&fov=40&heading=45&pitch=4" -O myStreetView02.jpg
+
+wget "https://maps.googleapis.com/maps/api/streetview?size=800x600&location=37.7728249509386,-122.406590353307
+&fov=40&heading=90&pitch=4" -O myStreetView03.jpg
+
+wget "https://maps.googleapis.com/maps/api/streetview?size=800x600&location=37.7728249509386,-122.406590353307
+&fov=40&heading=135&pitch=4" -O myStreetView04.jpg
+
+wget "https://maps.googleapis.com/maps/api/streetview?size=800x600&location=37.7728249509386,-122.406590353307
+&fov=40&heading=180&pitch=4" -O myStreetView05.jpg
+
+wget "https://maps.googleapis.com/maps/api/streetview?size=800x600&location=37.7728249509386,-122.406590353307
+&fov=40&heading=225&pitch=4" -O myStreetView06.jpg
+
+wget "https://maps.googleapis.com/maps/api/streetview?size=800x600&location=37.7728249509386,-122.406590353307
+&fov=40&heading=270&pitch=4" -O myStreetView07.jpg
+
+wget "https://maps.googleapis.com/maps/api/streetview?size=800x600&location=37.7728249509386,-122.406590353307
+&fov=40&heading=315&pitch=4" -O myStreetView08.jpg
+
+wget "https://maps.googleapis.com/maps/api/streetview?size=800x600&location=37.7728249509386,-122.406590353307
+&fov=40&heading=360&pitch=4" -O myStreetView09.jpg
---
+++ mapMarkers.sh
@@ -0,0 +1,1 @@
+wget "https://maps.googleapis.com/maps/api/staticmap?markers=37.7728249509386,-122.406590353307&zoom=12&size=400x400" -O myMap.jpg
